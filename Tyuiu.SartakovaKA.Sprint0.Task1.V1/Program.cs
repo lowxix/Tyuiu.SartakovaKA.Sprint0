30cf9ba1c31ed809d989098c1a190e684824e2e2
lowxix <resizxss@mail.ru>
2026-09-09 23:13:22 +0500

    Task1

---
+++ Tyuiu.SartakovaKA.Sprint0.Task1.V1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Tyuiu.SartakovaKA.Sprint0.Task1.V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            string name = "Ксения";
+            int age = 18;
+
+            Console.WriteLine("Привет, меня зовут " + name + " мне " + age + " лет.");
+
+            Console.ReadKey();
+        }
+    }
+}
