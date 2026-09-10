6dafae5014c4a344a8c5d57d72a8b388ea448d7b
lowxix <resizxss@mail.ru>
2026-09-10 22:00:39 +0500

    Task5

---
+++ Tyuiu.SartakovaKA.Sprint0.Task5.V1/Program.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.SartakovaKA.Sprint0.Task5.V1.Lib;
+namespace Tyuiu.SartakovaKA.Sprint0.Task5.V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A + B = " + DataService.Addition(1, 5));
+            Console.WriteLine("A - B = " + DataService.Subtraction(10, 5));
+            Console.WriteLine("A / B = " + DataService.Division(9, 3));
+            Console.WriteLine("A * B = " + DataService.Multiplication(10, 5));
+            Console.ReadKey();
+        }
+    }
+
+}
