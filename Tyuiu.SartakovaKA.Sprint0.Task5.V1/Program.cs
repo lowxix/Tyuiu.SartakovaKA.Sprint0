aa1b984b1ab3abc27b26603f8794dbdb5f702023
SartakovaKA <resizxss@mail.ru>
2026-09-10 22:04:02 +0500

    Update Program.cs

--- Tyuiu.SartakovaKA.Sprint0.Task5.V1/Program.cs
+++ Tyuiu.SartakovaKA.Sprint0.Task5.V1/Program.cs
@@ -7,7 +7,7 @@ namespace Tyuiu.SartakovaKA.Sprint0.Task5.V1
         {
             Console.WriteLine("A + B = " + DataService.Addition(1, 5));
             Console.WriteLine("A - B = " + DataService.Subtraction(10, 5));
-            Console.WriteLine("A / B = " + DataService.Division(9, 3));
+            Console.WriteLine("A / B = " + DataService.Division(9, 0));
             Console.WriteLine("A * B = " + DataService.Multiplication(10, 5));
             Console.ReadKey();
         }
