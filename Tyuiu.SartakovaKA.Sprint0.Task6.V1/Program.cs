9e8aef1de64d498cb6eb612db2cfcaee97841ef4
SartakovaKA <resizxss@mail.ru>
2026-09-14 20:38:47 +0500

    Add files via upload
    Task6

---
+++ Tyuiu.SartakovaKA.Sprint0.Task6.V1/Program.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.SartakovaKA.Sprint0.Task6.V1.Lib;
+namespace Tyuiu.SartakovaKA.Sprint0.Task6.V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arraynumbers = new int[] { 1, 2, 3, 4, 5 };
+            Console.WriteLine("Сумма элементов массива " + DataService.AdditionArray(arraynumbers));
+            Console.WriteLine("Разность элементов массива " + DataService.SubtractionArray(arraynumbers));
+            Console.WriteLine("Произведение элементов массива " + DataService.MultiplicationArray(arraynumbers));
+            Console.ReadKey();
+        }
+
+    }
+}    
